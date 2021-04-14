57fe45be58e07231da8b1efa27926328eb0343d0
wcoltonweeks <wcoltonweeks@outlook.com>
2021-04-14 11:10:26 -0500

    Implemented_Identity_Samples

--- SATApplication.UI.MVC/Startup.cs
+++ SATApplication.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(SATApplication.UI.MVC.Startup))]
 namespace SATApplication.UI.MVC
 {
     public partial class Startup
